fc840ebaf3b924d73eaec51dc4e9fa25a7b82d70
Aman Sharma <75797944+2020kucp1076@users.noreply.github.com>
2024-12-11 00:56:28 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/Snapgram-deploy/",
+  // base:"/Snapgram-deploy/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
